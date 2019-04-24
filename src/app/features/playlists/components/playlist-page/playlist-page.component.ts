bb0c39debd4f62ebd1fb4354104bc7d7f866b1a9
Alexandru.Bunea <alexandru.bunea@pitechnologies.ro>
2019-04-24 12:35:16 +0300

    feat(playlist): autocomplete songs youtube API

--- src/app/features/playlists/components/playlist-page/playlist-page.component.ts
+++ src/app/features/playlists/components/playlist-page/playlist-page.component.ts
@@ -1,8 +1,11 @@
 import { Component, AfterViewInit, EventEmitter } from '@angular/core';
-import { switchMap, combineLatest, pluck, map } from 'rxjs/operators';
+import { switchMap, combineLatest, pluck, map, startWith, debounceTime } from 'rxjs/operators';
 
 import { PlaylistsService } from '../../services/playlists.service';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import { SongsService } from '../../services/songs.service';
 
 @Component({
   selector: 'shuf-playlist-page',
@@ -13,6 +16,8 @@ export class PlaylistPageComponent implements AfterViewInit {
 
   fetchPlaylist = new EventEmitter();
   addSongEventEmitter = new EventEmitter();
+  autocompleteSongEmitter = new EventEmitter();
+  checkSongAvailability = new EventEmitter();
 
   playlist = this.fetchPlaylist
     .asObservable()
@@ -24,9 +29,12 @@ export class PlaylistPageComponent implements AfterViewInit {
       map((playlistId: any) => parseInt(playlistId))
     );
 
+  filteredOptions: Observable<any[]>;
+
   constructor(
     private readonly playlistsService: PlaylistsService,
-    private readonly route: ActivatedRoute
+    private readonly route: ActivatedRoute,
+    private readonly songsService: SongsService
   ) {
     this.addSongEventEmitter.asObservable()
       .pipe(
@@ -41,9 +49,33 @@ export class PlaylistPageComponent implements AfterViewInit {
       .subscribe(playlistId => {
         this.fetchPlaylist.emit(playlistId);
       });
+    this.filteredOptions = this.autocompleteSongEmitter.asObservable()
+      .pipe(
+        debounceTime(500),
+        switchMap(newTitle => this.songsService.getAll(newTitle))
+      );
   }
 
-  addSong(song) {
+  addSong(song, searchedText) {
     this.addSongEventEmitter.emit(song);
+    searchedText.reset();
+  }
+
+  titleChanged(newTitle) {
+    this.autocompleteSongEmitter.emit(newTitle);
+  }
+
+  isIncluded(songs, id): boolean {
+    return this.checkAvailability(songs, id);
+  }
+
+ private checkAvailability(songs, songId): boolean {
+    let availability = false;
+    songs.forEach( song => {
+     if (song.youtubeId === songId) {
+       availability = true;
+      }
+    });
+    return availability;
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormsModule} from '@angular/forms';
-import {MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, MatListModule, MatTableModule, MatCardModule} from '@angular/material';
+import {MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, MatListModule, MatTableModule, MatCardModule, MatAutocompleteModule} from '@angular/material';
 
 const MODULES = [CommonModule, FormsModule, MatFormFieldModule,
-   MatInputModule, MatIconModule, MatButtonModule, MatListModule, MatTableModule, MatCardModule];
+   MatInputModule, MatIconModule, MatButtonModule, MatListModule, MatTableModule, MatCardModule, MatAutocompleteModule];
 @NgModule({
   declarations: [],
   imports: [MODULES],
