aaf6ef5b5b04e8e799716fc17f3ebec4ac2d3212
Alexandru.Bunea <alexandru.bunea@pitechnologies.ro>
2019-05-07 17:53:23 +0300

    feat(playlist): add search bar functionality

--- src/app/features/playlists/components/playlists-page/playlists-page.component.ts
+++ src/app/features/playlists/components/playlists-page/playlists-page.component.ts
@@ -1,26 +1,24 @@
-import { Component, EventEmitter, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { switchMap } from 'rxjs/operators';
 
 import { PlaylistsService } from '../../services/playlists.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'shuf-playlists-page',
   templateUrl: './playlists-page.component.html',
   styleUrls: ['./playlists-page.component.scss']
 })
-export class PlaylistsPageComponent implements AfterViewInit {
+export class PlaylistsPageComponent {
 
-  fetchPlaylists = new EventEmitter();
-
-  playlists = this.fetchPlaylists
-  .asObservable()
-  .pipe(switchMap(() => this.playlistsService.getAll()));
+  playlists = this.activatedRoute.queryParamMap
+    .pipe(
+      switchMap(queryParams => this.playlistsService.filter(queryParams.get('q'))
+      ));
 
   constructor(
-    private readonly playlistsService: PlaylistsService
-  ) {}
-
-  ngAfterViewInit(): void {
-    this.fetchPlaylists.emit();
+    private readonly playlistsService: PlaylistsService,
+    private readonly activatedRoute: ActivatedRoute,
+  ) {
   }
 }
